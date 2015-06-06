2d7a40e48d376c8d6364fa5ee0fdfe8f4b76fcdd
Green Lightning <GreenLightning.git@googlemail.com>
2015-06-06 17:45:33 +0200

    Document HCPBorder.

--- src/eu/greenlightning/hypercubepdf/HCPBorder.java
+++ src/eu/greenlightning/hypercubepdf/HCPBorder.java
@@ -6,46 +6,152 @@ import java.util.Objects;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 
+/**
+ * An {@link HCPElement} wrapper which adds an empty border of a fixed size around the element.
+ * <p>
+ * The {@link #getWidth()} and {@link #getHeight()} methods return the sum of the element's size and the size of the
+ * border.
+ * <p>
+ * The element's {@link HCPElement#paint(PDPageContentStream, PDRectangle)} method will not be called if there is not
+ * enough space for the border.
+ * <p>
+ * This class is immutable.
+ *
+ * @author Green Lightning
+ */
 public class HCPBorder implements HCPElement {
 
+	/**
+	 * Wraps the element with an invisible border.
+	 * 
+	 * @param element not {@code null}
+	 * @return a wrapper for element
+	 * @throws NullPointerException if element is {@code null}
+	 */
 	public static HCPBorder getEmptyInstance(HCPElement element) {
 		return new HCPBorder(element, 0, 0, 0, 0);
 	}
 
+	/**
+	 * Wraps the element with a border with the given size on the top.
+	 * 
+	 * @param element not {@code null}
+	 * @param top {@literal >= 0}
+	 * @return a wrapper for element with the given border size
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if top is {@literal < 0}
+	 */
 	public static HCPBorder getTopInstance(HCPElement element, float top) {
 		return new HCPBorder(element, top, 0, 0, 0);
 	}
 
+	/**
+	 * Wraps the element with a border with the given size on the right.
+	 * 
+	 * @param element not {@code null}
+	 * @param right {@literal >= 0}
+	 * @return a wrapper for element with the given border size
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if right is {@literal < 0}
+	 */
 	public static HCPBorder getRightInstance(HCPElement element, float right) {
 		return new HCPBorder(element, 0, right, 0, 0);
 	}
 
+	/**
+	 * Wraps the element with a border with the given size on the bottom.
+	 * 
+	 * @param element not {@code null}
+	 * @param bottom {@literal >= 0}
+	 * @return a wrapper for element with the given border size
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if bottom is {@literal < 0}
+	 */
 	public static HCPBorder getBottomInstance(HCPElement element, float bottom) {
 		return new HCPBorder(element, 0, 0, bottom, 0);
 	}
 
+	/**
+	 * Wraps the element with a border with the given size on the left.
+	 * 
+	 * @param element not {@code null}
+	 * @param left {@literal >= 0}
+	 * @return a wrapper for element with the given border size
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if left is {@literal < 0}
+	 */
 	public static HCPBorder getLeftInstance(HCPElement element, float left) {
 		return new HCPBorder(element, 0, 0, 0, left);
 	}
 
+	/**
+	 * Wraps the element with a border with the given size on all sides.
+	 * 
+	 * @param element not {@code null}
+	 * @param size {@literal >= 0}
+	 * @return a wrapper for element with the given border size
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if size is {@literal < 0}
+	 */
 	public static HCPBorder getAllSidesInstance(HCPElement element, float size) {
 		return new HCPBorder(element, size, size, size, size);
 	}
-	
+
+	/**
+	 * Wraps the element with a border with the {@code horizontal} size on the left and right.
+	 * 
+	 * @param element not {@code null}
+	 * @param horizontal {@literal >= 0}
+	 * @return a wrapper for element with the given border sizes
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if horizontal is {@literal < 0}
+	 */
 	public static HCPBorder getHorizontalInstance(HCPElement element, float horizontal) {
 		return new HCPBorder(element, 0, horizontal, 0, horizontal);
 	}
 
+	/**
+	 * Wraps the element with a border with the {@code vertical} size on the top and bottom.
+	 * 
+	 * @param element not {@code null}
+	 * @param vertical {@literal >= 0}
+	 * @return a wrapper for element with the given border sizes
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if vertical is {@literal < 0}
+	 */
 	public static HCPBorder getVerticalInstance(HCPElement element, float vertical) {
 		return new HCPBorder(element, vertical, 0, vertical, 0);
 	}
 
+	/**
+	 * Wraps the element with a border with the {@code horizontal} size on the left an right and the {@code vertical}
+	 * size on the top and bottom.
+	 * 
+	 * @param element not {@code null}
+	 * @param horizontal {@literal >= 0}
+	 * @param vertical {@literal >= 0}
+	 * @return a wrapper for element with the given border sizes
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if any size is {@literal < 0}
+	 */
 	public static HCPBorder getHorizontalVerticalInstance(HCPElement element, float horizontal, float vertical) {
 		return new HCPBorder(element, vertical, horizontal, vertical, horizontal);
 	}
 
-	public static HCPBorder getTopRightBottomLeftInstance(HCPElement element, float top, float right,
-			float bottom, float left) {
+	/**
+	 * Wraps the element with a border with a different size on each size, as specified.
+	 * 
+	 * @param element not {@code null}
+	 * @param top {@literal >= 0}
+	 * @param right {@literal >= 0}
+	 * @param bottom {@literal >= 0}
+	 * @param left {@literal >= 0}
+	 * @return a wrapper for element with the given border sizes
+	 * @throws NullPointerException if element is {@code null}
+	 * @throws IllegalArgumentException if any size is {@literal < 0}
+	 */
+	public static HCPBorder getTopRightBottomLeftInstance(HCPElement element, float top, float right, float bottom,
+		float left) {
 		return new HCPBorder(element, top, right, bottom, left);
 	}
 
@@ -62,8 +168,7 @@ public class HCPBorder implements HCPElement {
 
 	private float checkSize(float size, String name) {
 		if (size < 0)
-			throw new IllegalArgumentException(name + " must be equal to or greater than zero, but was "
-					+ size + ".");
+			throw new IllegalArgumentException(name + " must be equal to or greater than zero, but was " + size + ".");
 		return size;
 	}
 
