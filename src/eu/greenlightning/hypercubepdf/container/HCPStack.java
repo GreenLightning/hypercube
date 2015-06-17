bfda16038be841e5d83d6828e7b1804040dbd752
Green Lightning <GreenLightning.git@googlemail.com>
2015-06-17 17:30:23 +0200

    Document HCPStack.

--- src/eu/greenlightning/hypercubepdf/container/HCPStack.java
+++ src/eu/greenlightning/hypercubepdf/container/HCPStack.java
@@ -8,14 +8,39 @@ import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 
 import eu.greenlightning.hypercubepdf.HCPElement;
 
+/**
+ * Paints multiple {@link HCPElement}s on top of each other. The elements are painted in the order they are
+ * provided in, i.&nbsp;e. the first element is painted first and the last element is painted last
+ * (bottom-to-top ordering).
+ * <p>
+ * The width of the widest and the height of the highest element are used as the size of the stack.
+ * <p>
+ * This class is immutable.
+ * 
+ * @author Green Lightning
+ */
 public class HCPStack implements HCPElement {
 
 	private final HCPElements elements;
 
+	/**
+	 * Creates an {@link HCPStack} from a {@link Collection} of elements.
+	 * 
+	 * @param elements not {@code null}; must not contain {@code null}
+	 * @throws NullPointerException if elements is {@code null}
+	 * @throws IllegalArgumentException if elements contains {@code null}
+	 */
 	public HCPStack(Collection<HCPElement> elements) {
 		this.elements = new HCPElements(elements);
 	}
 
+	/**
+	 * Creates an {@link HCPStack} instance from an array or from a varargs list of elements.
+	 * 
+	 * @param elements not {@code null}; must not contain {@code null}
+	 * @throws NullPointerException if elements is {@code null}
+	 * @throws IllegalArgumentException if elements contains {@code null}
+	 */
 	public HCPStack(HCPElement... elements) {
 		this.elements = new HCPElements(elements);
 	}
