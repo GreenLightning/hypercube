552e03fac5753a012fa5a3cdc4ce0817860dafa9
Green Lightning <GreenLightning.git@googlemail.com>
2015-06-27 12:39:20 +0200

    Updated layout package documentation.

--- src/eu/greenlightning/hypercubepdf/layout/package-info.java
+++ src/eu/greenlightning/hypercubepdf/layout/package-info.java
@@ -1,2 +1,8 @@
-/** One-dimensional layout algorithms used by containers. */
+/**
+ * One-dimensional layout algorithms used by containers.
+ * <p>
+ * These layout algorithms are independent of the axis on which they are applied. They are provided with the
+ * {@code size}s of the elements which can either be their {@code width}s or their their {@code height}s, depending on
+ * whether the container wants to apply the layout horizontally or vertically.
+ */
 package eu.greenlightning.hypercubepdf.layout;
