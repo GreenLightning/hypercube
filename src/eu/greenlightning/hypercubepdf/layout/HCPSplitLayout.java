d0503183a621a5fba964c3a8ba8a5fd75d5c2967
Green Lightning <GreenLightning.git@googlemail.com>
2015-06-28 12:24:57 +0200

    Document HCPSplitLayout.

--- src/eu/greenlightning/hypercubepdf/layout/HCPSplitLayout.java
+++ src/eu/greenlightning/hypercubepdf/layout/HCPSplitLayout.java
@@ -1,13 +1,34 @@
 package eu.greenlightning.hypercubepdf.layout;
 
+/**
+ * A layout algorithm which splits the available space equally among the elements, regardless of their preferred size.
+ * <p>
+ * Always paints all elements unless there is not enough space for the spacing alone, in which case nothing is painted.
+ * <p>
+ * This class is immutable.
+ * 
+ * @author Green Lightning
+ */
 public class HCPSplitLayout extends HCPSpacedLayout {
 
 	private static final HCPSplitLayout NO_SPACING = new HCPSplitLayout(0);
 
+	/**
+	 * Returns an {@link HCPSplitLayout} instance which places elements next to each other.
+	 * 
+	 * @return an instance with no spacing
+	 */
 	public static HCPSplitLayout getInstance() {
 		return NO_SPACING;
 	}
 
+	/**
+	 * Returns an {@link HCPSplitLayout} instance which maintains the specified spacing between two adjacent elements.
+	 * 
+	 * @param spacing must be {@literal >= 0}
+	 * @return an instance with the specified spacing
+	 * @throws IllegalArgumentException if spacing is {@literal < 0}
+	 */
 	public static HCPSplitLayout getInstance(float spacing) {
 		return spacing == 0 ? NO_SPACING : new HCPSplitLayout(spacing);
 	}
