8e22d9bcb5ee92c0ec1b8edecfce02de848256d4
Green Lightning <GreenLightning.git@googlemail.com>
2015-06-30 20:45:49 +0200

    Document HCPFlowLayout.

--- src/eu/greenlightning/hypercubepdf/layout/HCPFlowLayout.java
+++ src/eu/greenlightning/hypercubepdf/layout/HCPFlowLayout.java
@@ -1,13 +1,33 @@
 package eu.greenlightning.hypercubepdf.layout;
 
+/**
+ * A layout algorithm which always uses the elements preferred size. If an element's size is larger than the remaining
+ * space, the element is painted inside the remaining space and all further elements are ignored.
+ * <p>
+ * This class is immutable.
+ * 
+ * @author Green Lightning
+ */
 public class HCPFlowLayout extends HCPSpacedLayout {
 
 	private final static HCPFlowLayout NO_SPACING = new HCPFlowLayout(0);
 
+	/**
+	 * Returns an {@link HCPFlowLayout} instance which places elements next to each other.
+	 * 
+	 * @return an instance with no spacing
+	 */
 	public static HCPFlowLayout getInstance() {
 		return NO_SPACING;
 	}
 
+	/**
+	 * Returns an {@link HCPFlowLayout} instance which maintains the specified spacing between two adjacent elements.
+	 * 
+	 * @param spacing must be {@literal >= 0}
+	 * @return an instance with the specified spacing
+	 * @throws IllegalArgumentException if spacing is {@literal < 0}
+	 */
 	public static HCPFlowLayout getInstance(float spacing) {
 		return spacing == 0 ? NO_SPACING : new HCPFlowLayout(spacing);
 	}
